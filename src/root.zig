fa48a114397f27235abbaf3d21915cc0636a3612
Peyman Mortazavi <pey.mortazavi@gmail.com>
2025-12-29 15:50:51 -0500

    remove invalid Column reference

--- src/root.zig
+++ src/root.zig
@@ -2,7 +2,6 @@ const iterator = @import("iterator.zig");
 const emitter = @import("emitter.zig");
 const simd = @import("simd.zig");
 
-pub const Column = iterator.Column;
 pub const Csv = iterator.Csv;
 pub const Iterator = Csv(.{});
 pub const Emitter = emitter.Emitter;
